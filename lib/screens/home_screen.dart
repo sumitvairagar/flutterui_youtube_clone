bc765cbd012b48d63e401cc326bec215973dc969
sumitvairagar <46483045+sumitvairagar@users.noreply.github.com>
2021-08-04 12:23:12 +0530

    Added video card to display the video

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_youtube_ui/widgets/video_card.dart';
+import 'package:flutter_youtube_ui/widgets/widgets.dart';
+
+import '../data.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          CustomSliverAppBar(),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+                (context, index){
+                  final video = videos[index];
+                  return VideoCard(video: video);
+                },
+              childCount: videos.length,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
---
+++ lib/widgets/video_card.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:timeago/timeago.dart' as timeago;
+import '../data.dart';
+
+class VideoCard extends StatelessWidget {
+  const VideoCard({Key? key, required this.video}) : super(key: key);
+
+  final Video video;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(
+          children: [
+            Image.network(
+              video.thumbnailUrl,
+              height: 220.0,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            Positioned(
+              bottom: 8.0,
+              right: 8.0,
+              child: Container(
+                padding: const EdgeInsets.all(4.0),
+                color: Colors.black,
+                child: Text(
+                  video.duration,
+                  style: Theme.of(context)
+                      .textTheme
+                      .caption!
+                      .copyWith(color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              GestureDetector(
+                child: CircleAvatar(
+                  foregroundImage: NetworkImage(video.author.profileImageUrl),
+                ),
+                onTap: () => {print("User account")},
+              ),
+              const SizedBox(width: 8.0,),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Flexible(
+                      child: Text(
+                        video.title,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.bodyText1!.copyWith(fontSize: 15.0),
+                      ),
+                    ),
+                    Flexible(
+                      child: Text(
+                        '${video.author.username} * ${video.viewCount} views * ${timeago.format(video.timestamp)}',
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: Theme.of(context).textTheme.caption!.copyWith(fontSize: 14.0),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              GestureDetector(
+                onTap: () => {},
+                  child: Icon(Icons.more_vert, size: 20.0,)),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
